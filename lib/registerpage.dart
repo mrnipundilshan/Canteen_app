b3fb1459c68212f9f87c888424bb16b4a6232a9d
nipun <nipundilshan991@gmail.com>
2024-06-24 21:58:02 +0530

    newchanges

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -6,6 +6,8 @@ class Registerpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController mobiletextcontroller = TextEditingController();
+
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -42,6 +44,7 @@ class Registerpage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(30.0),
               ),
               child: TextField(
+                controller: mobiletextcontroller,
                 style: TextStyle(
                     fontWeight: FontWeight.w800,
                     color: const Color.fromRGBO(60, 121, 98, 1.0),
@@ -50,6 +53,58 @@ class Registerpage extends StatelessWidget {
                 cursorColor: const Color.fromRGBO(60, 121, 98, 1.0),
                 decoration: const InputDecoration(
                   hintText: "Mobile Number",
+                  hintStyle: TextStyle(
+                      color: Color.fromRGBO(60, 121, 98, 1.0),
+                      fontWeight: FontWeight.bold),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.01,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              width: size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(119, 187, 162, 1.0),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: TextField(
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: const Color.fromRGBO(60, 121, 98, 1.0),
+                    fontSize: size.height * 0.02),
+                textAlign: TextAlign.center,
+                cursorColor: const Color.fromRGBO(60, 121, 98, 1.0),
+                decoration: const InputDecoration(
+                  hintText: "Name",
+                  hintStyle: TextStyle(
+                      color: Color.fromRGBO(60, 121, 98, 1.0),
+                      fontWeight: FontWeight.bold),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.01,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              width: size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(119, 187, 162, 1.0),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: TextField(
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: const Color.fromRGBO(60, 121, 98, 1.0),
+                    fontSize: size.height * 0.02),
+                textAlign: TextAlign.center,
+                cursorColor: const Color.fromRGBO(60, 121, 98, 1.0),
+                decoration: const InputDecoration(
+                  hintText: "Name",
                   hintStyle: TextStyle(
                       color: Color.fromRGBO(60, 121, 98, 1.0),
                       fontWeight: FontWeight.bold),
