4615afe60156023febd3bff578b56d50736f6154
nipun <nipundilshan991@gmail.com>
2024-07-11 22:32:29 +0530

    food_card

---
+++ lib/pages/food_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class food_card extends StatefulWidget {
+  const food_card({super.key});
+
+  @override
+  State<food_card> createState() => _food_cardState();
+}
+
+class _food_cardState extends State<food_card> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,66 +1,32 @@
-import 'dart:convert';
-
 import 'package:canteen/backgrounds/signup_bg.dart';
-import 'package:canteen/config/config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:flutter/widgets.dart';
-
-class menuitem {
-  late final String id;
-  late final String name;
-  late final String price;
-  late final int visible;
-
-  menuitem(
-      {required this.id,
-      required this.name,
-      required this.price,
-      required this.visible});
-
-  factory menuitem.fromJson(Map<String, dynamic> json) {
-    return menuitem(
-      id: json['_id'],
-      name: json['name'],
-      price: json['price'],
-      visible: json['visible'],
-    );
-  }
-}
-
-Future<List<menuitem>> fetchmenuitems() async {
-  final response = await http.get(Uri.parse(menulist));
-
-  if (response.statusCode == 200) {
-    List jsonResponse = json.decode(response.body);
-    return jsonResponse.map((item) => menuitem.fromJson(item)).toList();
-  } else {
-    throw Exception('Failed to load menu items');
-  }
-}
+import 'package:http/http.dart' as http;
+import 'package:canteen/config/config.dart';
 
 class menupage extends StatefulWidget {
-  const menupage({super.key});
+  final token;
+  const menupage({@required this.token, Key? key}) : super(key: key);
 
   @override
   State<menupage> createState() => _menupageState();
 }
 
 class _menupageState extends State<menupage> {
-  late Future<List<menuitem>> futuremenuitems;
+  late String mobile_number;
 
   @override
-  void initstate() {
+  void initState() {
+    // TODO: implement initState
     super.initState();
-    futuremenuitems = fetchmenuitems();
+    Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(widget.token);
+
+    mobile_number = jwtDecodedToken['mobile_number'];
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       body: Background(
         child: SingleChildScrollView(
@@ -100,6 +66,7 @@ class _menupageState extends State<menupage> {
                           color: const Color.fromRGBO(60, 121, 98, 1.0)),
                     ),
                     Container(
+                      child: Text("N"),
                       padding:
                           EdgeInsets.symmetric(vertical: size.height * 0.05),
                       width: size.width * 0.9,
@@ -118,52 +85,3 @@ class _menupageState extends State<menupage> {
     );
   }
 }
-// class menupage extends StatefulWidget {
-//   final token;
-//   const menupage({@required this.token, Key? key}) : super(key: key);
-
-//   @override
-//   State<menupage> createState() => _menupageState();
-// }
-
-// class _menupageState extends State<menupage> {
-//   late String mobile_number;
-
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(widget.token);
-
-//     mobile_number = jwtDecodedToken['mobile_number'];
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     Size size = MediaQuery.of(context).size;
-//     return Background(
-//       child: SingleChildScrollView(
-//         child: Container(
-//           width: double.infinity,
-//           height: size.height,
-//           child: Column(
-//             children: <Widget>[
-//               //Text(mobile_number),
-//               Text("sfdsd"),
-//               Text(
-//                 "Login",
-//                 style: TextStyle(
-//                     fontSize: size.width * 0.1,
-//                     fontWeight: FontWeight.bold,
-//                     color: const Color.fromRGBO(60, 121, 98, 1.0)),
-//               ),
-//               SizedBox(
-//                 height: size.height * 0.02,
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
